2bef02e58a1cd9913ba81cb2e17173aebb572ec8
hd <hd@iMac-de-hd.local>
2022-08-23 22:11:11 -0300

    alex bebidas

--- src/components/ano.jsx
+++ src/components/ano.jsx
@@ -1,10 +1,5 @@
 function DataH() {    
-    function dateH (){
-   
-    // const data = new Date();
-    const ano = "data.getFullYear()";
-    return ano
-    }
+    
     return (         
     <>
         <span>2022 - </span>
